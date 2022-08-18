d0aacdbe7a9a73f09dc009ff66de1831373e37a0
Davy <davy.marthely@ynov.com>
2022-08-18 20:21:04 +0200

    Partiellement fini

---
+++ portfolio.js
@@ -0,0 +1,40 @@
+document.getElementById('one').onmouseenter = function (){
+    document.getElementById('Lolpic').style.display = 'none';
+    document.getElementById('Lol').style.display = 'block';
+}
+document.getElementById('one').onmouseleave = function (){
+    document.getElementById('Lolpic').style.display = 'block';
+    document.getElementById('Lol').style.display = 'none';
+}
+document.getElementById('two').onmouseenter = function (){
+    document.getElementById('shoppic').style.display = 'none';
+    document.getElementById('shop').style.display = 'block';
+}
+document.getElementById('two').onmouseleave = function (){
+    document.getElementById('shoppic').style.display = 'block';
+    document.getElementById('shop').style.display = 'none';
+}
+document.getElementById('three').onmouseenter = function (){
+    document.getElementById('quizzpic').style.display = 'none';
+    document.getElementById('quizz').style.display = 'block';
+}
+document.getElementById('three').onmouseleave = function (){
+    document.getElementById('quizzpic').style.display = 'block';
+    document.getElementById('quizz').style.display = 'none';
+}
+document.getElementById('four').onmouseenter = function (){
+    document.getElementById('meteopic').style.display = 'none';
+    document.getElementById('meteo').style.display = 'block';
+}
+document.getElementById('four').onmouseleave = function (){
+    document.getElementById('meteopic').style.display = 'block';
+    document.getElementById('meteo').style.display = 'none';
+}
+document.getElementById('five').onmouseenter = function (){
+    document.getElementById('workpic').style.display = 'none';
+    document.getElementById('work').style.display = 'block';
+}
+document.getElementById('five').onmouseleave = function (){
+    document.getElementById('workpic').style.display = 'block';
+    document.getElementById('work').style.display = 'none';
+}
